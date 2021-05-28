e302a3874c5ef5e32db98c10528e6129379c661e
charlesdwright <71340308+charlesdwright@users.noreply.github.com>
2021-05-28 12:49:22 -0400

    Eliminate code warnings in hopes of passing heroku release phase.

--- src/app.py
+++ src/app.py
@@ -3,33 +3,36 @@ import os
 from part3 import get_sentiment
 from flask import Flask
 from flask import request
-#from flask_cors import CORS, cross_origin
 import datetime
 
 app = Flask(__name__)
-#cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-retval ='sentiment_demo_1'
+retval = 'sentiment_demo_1'
 retval = retval + '<br>' + datetime.datetime.now().replace(microsecond=0).isoformat(' ')
 retval = retval + '<br>' + '<br>'
+
 
 @app.route('/')
 def hello_whale():
     return retval + 'Whale, Hello there!'
 
+
 @app.route('/blah')
 def blah_yadda():
     return retval + 'Blah blah yadda yadda'
 
-@app.route('/predict', methods = ['GET', 'POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
+
     if request.method == 'GET':
-        input = request.args.get('input')
+        theinput = request.args.get('input')
     else:
-        input = request.get_json(force=True)['input']
-    if not input:
+        theinput = request.get_json(force=True)['input']
+    if not theinput:
         return 'No input value found'
-    return retval + get_sentiment(input)
+    return retval + get_sentiment(theinput)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
